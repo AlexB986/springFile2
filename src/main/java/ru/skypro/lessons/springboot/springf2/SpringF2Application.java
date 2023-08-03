8644ce25212c90a9c309d6c187d2e9b4c32b3b05
Alex <borovinskihalex986@gmail.com>
2023-08-03 17:59:59 +0500

    var 2.0

---
+++ src/main/java/ru/skypro/lessons/springboot/springf2/SpringF2Application.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.springboot.springf2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringF2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringF2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/skypro/lessons/springboot/springf2/SpringF2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.springboot.springf2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringF2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
